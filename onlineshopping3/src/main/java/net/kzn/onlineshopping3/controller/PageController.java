661d43d1d04ab1903534020ef7d21901e9271972
Shukri Manaf <shukrimanaf95@gmail.com>
2018-10-05 17:33:16 +0800

    Forth

--- onlineshopping3/src/main/java/net/kzn/onlineshopping3/controller/PageController.java
+++ onlineshopping3/src/main/java/net/kzn/onlineshopping3/controller/PageController.java
@@ -3,7 +3,6 @@ package net.kzn.onlineshopping3.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,13 +26,13 @@ public class PageController {
 //		return mv;
 //	}
 	
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting")String greeting){
-		if(greeting == null) {
-			greeting="Hello there";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
-	}
+//	@RequestMapping(value="/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting")String greeting){
+//		if(greeting == null) {
+//			greeting="Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
 }
